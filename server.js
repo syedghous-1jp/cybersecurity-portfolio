9d72254e8eadc2aee6b607bb8df7cf36acf6c038
Syed GHOUS <syedghxous177@gmail.com>
2026-06-12 14:57:49 +0400

    Add a route to confirm server is running

--- server.js
+++ server.js
@@ -51,4 +51,7 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+  app.get("/", (req, res) => {
+  res.send("AI Chatbot is running 🚀");
+});
 });
